2e6edb145229d27e67f4235345d5b6a30c8c1ac7
Sophie <socarboni@gmail.com>
2022-02-06 18:02:04 +0100

    just started

---
+++ day0/ex00/srcs/Megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/06 17:40:34 by scarboni          #+#    #+#             */
+/*   Updated: 2022/02/06 17:47:32 by scarboni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+std::string	string_to_upper(std::string string);
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+		std::cout << string_to_upper(argv[i]);
+	std::cout << std::endl;
+	return 0;
+}
+
+std::string	string_to_upper(std::string string)
+{
+	size_t	len(string.size());
+
+	for (size_t i = 0; i < len; i++)
+		string[i] = (char)toupper(string[i]);
+	return string;
+}
